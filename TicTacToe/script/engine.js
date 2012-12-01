fb28055481c6fdfcfba4e2ca7ff52ac55b2a7a2b
OGHX <oghx@LINUX.(none)>
2012-12-02 01:39:22 +0200

    CSS and game board initialized!

--- TicTacToe/script/engine.js
+++ TicTacToe/script/engine.js
@@ -4,3 +4,20 @@
  * Date: 11/29/12
  * Time: 2:51 AM
  */
+
+var TicTacToe = {};
+
+
+TicTacToe.GameEngine = function(){
+   /* var data = document.getElementById("textBox").value;
+    var box = document.createElement('div');
+    box.setAttribute('class', 'box');
+
+    for(var i = 0; i < data; i++){
+        document.getElementById("mainBoard").appendChild(box);
+        console.log('element added!');
+        console.log(i);
+    }*/
+
+
+};
